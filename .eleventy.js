c2d2df39515ba77d9b064657a6ccf816a6c98935
evgenvandev <46090955+evgenvandev@users.noreply.github.com>
2024-12-25 11:19:25 +0000

    6

--- .eleventy.js
+++ .eleventy.js
@@ -115,7 +115,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("/example-node-eleventy/src/static/img");
+  eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
